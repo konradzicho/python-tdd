d744291c3f9f8ae0c36e32038d59ceca7e71610d
konradzicho <konradzicho@gmail.com>
2017-10-25 23:40:08 +0200

    adding tests and implementation

--- src/invert_list.py
+++ src/invert_list.py
@@ -1,5 +1,11 @@
 
 
 def invert_list(input_list):
-    pass
+    N = len(input_list)
+    if N >1:
+        buffer = input_list[0]
+        input_list[0] = input_list[N - 1]
+        input_list[N - 1] = buffer
+
+    return input_list
 
--- test/test_invert_list.py
+++ test/test_invert_list.py
@@ -6,5 +6,26 @@ from src.invert_list import invert_list
 class TestUtils(unittest.TestCase):
 
     def test_invert_empty_list(self):
-
         self.assertEqual([], invert_list([]))
+
+    def test_invert_list_with_two_elements(self):
+        self.assertEqual([2,1], invert_list([1,2]))
+
+    def test_invert_list_with_1_4(self):
+        self.assertEqual([4,1], invert_list([1,4]))
+
+    def test_invert_list_with_3_1(self):
+        self.assertEqual([1,3], invert_list([3,1]))
+
+    def test_invert_list_with_one_element(self):
+        self.assertEqual([1], invert_list([1]))
+
+    def test_invert_list_with_three_elements(self):
+        ll = [1, 2, 3]
+        self.assertEqual([3,2,1], invert_list(ll))
+        self.assertEqual(ll, [1,2,3])
+
+#    def test_invert_none_argument(self):
+#        self.assertEqual([], invert_list([]))
+#       self.assertRaises(ValueError("no argument"), invert_list(None))
+
